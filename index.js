863b632506876e257f0dfe124d8595b547ffc88d
zhengwenbing <zhengwenbing@meituan.com>
2017-12-05 11:13:20 +0800

    ignore node_modules and .node

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+const path = require('path');
+
 function clearRequireCache(id) {
   var mod = require.cache[id]
   if (!mod) {
     return
   }
+  if (id.split(path.sep).indexOf('node_modules') !== -1) {
+    return
+  }
+  if (path.extname(id) === '.node') {
+    return;
+  }
   var mods = mod.children.map(function(child) {
     return child.id
   })
-  mods.forEach(clearRequireCache)
   delete require.cache[id]
+  mods.forEach(clearRequireCache)
 }
 
 module.exports = clearRequireCache
